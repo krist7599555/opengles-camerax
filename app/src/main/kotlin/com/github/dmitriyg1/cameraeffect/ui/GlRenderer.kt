5d93c0f71bd62abac9f217bc5d03b538bec25c92
krist7599555 <krist7599555@gmail.com>
2021-04-22 18:49:19 +0700

    hard code matrix is work

--- app/src/main/kotlin/com/github/dmitriyg1/cameraeffect/ui/GlRenderer.kt
+++ app/src/main/kotlin/com/github/dmitriyg1/cameraeffect/ui/GlRenderer.kt
@@ -14,8 +14,7 @@ import com.github.dmitriyg1.cameraeffect.util.toBitmap
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
-class GlRenderer(private val gLSurfaceView: GLSurfaceView) : GLSurfaceView.Renderer,
-  ImageAnalysis.Analyzer {
+class GlRenderer(private val gLSurfaceView: GLSurfaceView) : GLSurfaceView.Renderer, ImageAnalysis.Analyzer {
   private val textures = IntArray(2)
   private var square: Square? = null
 
@@ -27,12 +26,10 @@ class GlRenderer(private val gLSurfaceView: GLSurfaceView) : GLSurfaceView.Rende
   @Synchronized
   fun setImage(image: Bitmap) {
     this.image?.recycle()
-
     this.image = image
   }
 
   override fun onDrawFrame(p0: GL10?) {
-    generateSquare()
     generateTexture()
 
     if (effectContext == null) {
@@ -48,22 +45,15 @@ class GlRenderer(private val gLSurfaceView: GLSurfaceView) : GLSurfaceView.Rende
   override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
     GLES20.glViewport(0, 0, width, height)
     GLES20.glClearColor(0f, 0f, 0f, 1f)
-    generateSquare()
+    square = Square()
   }
 
   override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
   }
 
-  private fun generateSquare() {
-    if (square == null) {
-      square = Square()
-    }
-  }
-
   private fun generateTexture() {
     GLES20.glGenTextures(2, textures, 0)
     GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0])
-
 
     GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR)
     GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
--- app/src/main/kotlin/com/github/dmitriyg1/cameraeffect/ui/Square.kt
+++ app/src/main/kotlin/com/github/dmitriyg1/cameraeffect/ui/Square.kt
@@ -1,35 +1,95 @@
 package com.github.dmitriyg1.cameraeffect.ui
 
 import android.opengl.GLES20
+import android.opengl.GLES20.*
+import android.opengl.GLException
+import android.opengl.Matrix
+import android.util.Log
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
+
+fun floatarrayToBuffer(fa: FloatArray): FloatBuffer {
+  return ByteBuffer
+    .allocateDirect(fa.size * 4)
+    .order(ByteOrder.nativeOrder())
+    .asFloatBuffer()
+    .apply {
+      put(fa)
+      position(0)
+    }
+}
 
 class Square {
   private val vertices = floatArrayOf(-1f, -1f, 1f, -1f, -1f, 1f, 1f, 1f)
   private val textureVertices = floatArrayOf(0f, 1f, 1f, 1f, 0f, 0f, 1f, 0f)
 
+  private val arrow = floatArrayOf(
+    -.5f, -.5f, .5f, //
+    -.5f, .5f, .5f, //
+    .5f, .5f, .5f, //
+    .5f, -.5f, .5f, //
+    -.5f, -.5f, -.5f, //
+    -.5f, .5f, -.5f, //
+    .5f, .5f, -.5f, //
+    .5f, -.5f, -.5f  //
+  )
+
+  private val projectionMatrix = FloatArray(16).apply {
+//    val camera = FloatArray(16)
+//    val project = FloatArray(16)
+//    Matrix.setIdentityM(camera, 0)
+//    Matrix.perspectiveM(project, 0, 30f, 1f / 1f, 0.1f, 10f)
+//    Matrix.multiplyMM(this, 0, project, 0, camera, 0);
+//    Matrix.setIdentityM(this, 0)
+  }
+
   private lateinit var verticesBuffer: FloatBuffer
   private lateinit var textureBuffer: FloatBuffer
+  private lateinit var arrowBuffer: FloatBuffer
+  private lateinit var projectionMatrixBuffer : FloatBuffer
 
   private var vertexShader: Int = 0
   private var fragmentShader: Int = 0
   private var program: Int = 0
 
 
-  private val vertexShaderCode = "attribute vec4 aPosition;" +
+  private val vertexShaderCode =
+    "#version 100\n" +
+    "precision mediump float;" +
+      "precision mediump int;" +
+    "uniform int uMode;" +
+    "uniform mat4 uProjectionMatrix;" +
+    "attribute vec4 aPosition;" +
     "attribute vec2 aTexPosition;" +
     "varying vec2 vTexPosition;" +
+    "mat4 identity = mat4(1.0,0.0,0.0,0.5, 0.0,1.0,0.0,0.0, 0.0,0.0,1.0,0.0, 0.0,0.0,0.0,1.0);" +
     "void main() {" +
-    "  gl_Position = aPosition;" +
+    "  if (uMode == 1) {" +
+    "    gl_Position = aPosition;" +
+    "  } else {" +
+//    "    gl_Position = vec4(aPosition.x, aPosition.y, aPosition.z, 1.0);" +
+//      "    gl_Position = aPosition;" +
+//      "    gl_Position = vec4(aPosition.xyz, 1.0);" +
+//    "    gl_Position = vec4(-1.0,1.0,1.0,1.0) * vec4(aPosition.xyz, 1.0);" + // flip y
+      "    gl_Position = identity * vec4(aPosition.xyz, 1.0);" +
+    "  }" +
     "  vTexPosition = aTexPosition;" +
     "}"
 
-  private val fragmentShaderCode = "precision mediump float;" +
+  private val fragmentShaderCode =
+    "#version 100\n" +
+    "precision mediump float;" +
+      "precision mediump int;" +
+    "uniform int uMode;" +
     "uniform sampler2D uTexture;" +
     "varying vec2 vTexPosition;" +
     "void main() {" +
-    "  gl_FragColor = texture2D(uTexture, vTexPosition);" +
+    "  if (uMode == 1) {" +
+    "    gl_FragColor = texture2D(uTexture, vTexPosition);" +
+    "  } else {" +
+    "    gl_FragColor = vec4(1, 0, 0, 1);" +
+    "  }" +
     "}"
 
   init {
@@ -49,22 +109,75 @@ class Square {
     textureBuffer = buff.asFloatBuffer()
     textureBuffer.put(textureVertices)
     textureBuffer.position(0)
+
+    buff = ByteBuffer.allocateDirect(arrow.size * 4)
+    buff.order(ByteOrder.nativeOrder())
+    arrowBuffer = buff.asFloatBuffer()
+    arrowBuffer.put(arrow)
+    arrowBuffer.position(0)
+
+
+//    val p = FloatArray(16)
+//    Matrix.setIdentityM(p, 0)
+//    Matrix.transposeM(projectionMatrix, 0, p , 0);
+    Matrix.setIdentityM(projectionMatrix, 0)
+    buff = ByteBuffer.allocateDirect(projectionMatrix.size * 4)
+    buff.order(ByteOrder.nativeOrder())
+    projectionMatrixBuffer = buff.asFloatBuffer()
+    projectionMatrixBuffer.put(projectionMatrix)
+    projectionMatrixBuffer.position(0)
   }
 
   private fun initializeProgram() {
+    val result = IntArray(1)
+
     vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER)
     GLES20.glShaderSource(vertexShader, vertexShaderCode)
     GLES20.glCompileShader(vertexShader)
+    GLES20.glGetShaderiv(vertexShader, GLES20.GL_COMPILE_STATUS, result, 0)
+    if (result[0] == GL_FALSE) {
+      Log.e(
+        "CameraGLRendererBase", "Could not compile vertex shader: " + GLES20.glGetShaderInfoLog(
+          vertexShader
+        )
+      );
+      GLES20.glDeleteShader(vertexShader);
+      throw GLException(result[0], "Could not compile vertex shader")
+    }
 
     fragmentShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER)
     GLES20.glShaderSource(fragmentShader, fragmentShaderCode)
     GLES20.glCompileShader(fragmentShader)
+    GLES20.glGetShaderiv(fragmentShader, GLES20.GL_COMPILE_STATUS, result, 0)
+    if (result[0] == GL_FALSE) {
+      Log.e(
+        "CameraGLRendererBase", "Could not compile fragment shader:" + GLES20.glGetShaderInfoLog(
+          fragmentShader
+        )
+      );
+      GLES20.glDeleteShader(vertexShader)
+      GLES20.glDeleteShader(fragmentShader)
+      throw GLException(result[0], "Could not compile fragment shader")
+    }
 
     program = GLES20.glCreateProgram()
     GLES20.glAttachShader(program, vertexShader)
     GLES20.glAttachShader(program, fragmentShader)
 
     GLES20.glLinkProgram(program)
+    GLES20.glGetProgramiv(program, GL_LINK_STATUS, result, 0);
+    if (result[0] === GL_FALSE) {
+      val message = "Link program fail" + GLES20.glGetProgramInfoLog(program)
+      Log.e("CameraGLRendererBase", message)
+      throw GLException(result[0], message)
+    }
+
+    GLES20.glValidateProgram(program)
+    GLES20.glGetProgramiv(program, GLES20.GL_VALIDATE_STATUS, result, 0)
+    if (result[0] === GL_FALSE) {
+      val message = if (!GLES20.glIsProgram(program)) "Program handle deprecated!" else "Program do not validated!"
+      throw GLException(result[0], message)
+    }
   }
 
   fun draw(texture: Int) {
@@ -72,9 +185,15 @@ class Square {
     GLES20.glUseProgram(program)
     GLES20.glDisable(GLES20.GL_BLEND)
 
+    val modeHandle = GLES20.glGetUniformLocation(program, "uMode")
+    val projectionMatrixHandle = GLES20.glGetUniformLocation(program, "uProjectionMatrix")
     val positionHandle = GLES20.glGetAttribLocation(program, "aPosition")
     val textureHandle = GLES20.glGetUniformLocation(program, "uTexture")
     val texturePositionHandle = GLES20.glGetAttribLocation(program, "aTexPosition")
+
+    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+    GLES20.glUniform1i(modeHandle, 1) // texture
 
     GLES20.glVertexAttribPointer(texturePositionHandle, 2, GLES20.GL_FLOAT, false, 0, textureBuffer)
     GLES20.glEnableVertexAttribArray(texturePositionHandle)
@@ -86,7 +205,21 @@ class Square {
     GLES20.glVertexAttribPointer(positionHandle, 2, GLES20.GL_FLOAT, false, 0, verticesBuffer)
     GLES20.glEnableVertexAttribArray(positionHandle)
 
-    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
     GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+
+    GLES20.glDisableVertexAttribArray(positionHandle)
+
+    GLES20.glUniform1i(modeHandle, 2) // shader
+
+    println(projectionMatrixBuffer)
+    GLES20.glUniformMatrix4fv(projectionMatrixHandle, 0, false, projectionMatrixBuffer)
+
+    GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 0, arrowBuffer)
+    GLES20.glEnableVertexAttribArray(positionHandle)
+
+    GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+
+    GLES20.glDisableVertexAttribArray(positionHandle)
+
   }
 }
